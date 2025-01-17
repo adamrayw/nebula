d67747634f4c941026a5e08cfab1cf517f1bf1fe
Nabil Anugerah <nabilpangestu123@gmail.com>
2025-01-17 21:38:41 +0700

    Update: Enhance layout and typography in CategoriesIndicator component for better responsiveness and readability

--- src/pages/dashboard/components/CategoriesIndicator.tsx
+++ src/pages/dashboard/components/CategoriesIndicator.tsx
@@ -1,7 +1,13 @@
 import { Card } from "@/pages/core/components/design-system/ui/card";
 import { useFetchCategories } from "@/queries/useFetchFiles";
 import { ICategory } from "@/types/ICategory";
-import { FaFile, FaFileAudio, FaFileImage, FaFilePdf, FaFileVideo } from "react-icons/fa";
+import {
+  FaFile,
+  FaFileAudio,
+  FaFileImage,
+  FaFilePdf,
+  FaFileVideo,
+} from "react-icons/fa";
 
 const CategoriesIndicator = () => {
   const { data }: { data?: { data: ICategory[] } } = useFetchCategories() as {
@@ -17,13 +23,15 @@ const CategoriesIndicator = () => {
 
   return (
     <section className="my-10">
-      <div className="grid grid-cols-4 gap-x-4 place-items-center">
+      <div className="grid lg:grid-cols-4 md:grid-cols-2 grid-cols-2 gap-x-4 lg:gap-y-0 gap-y-2 place-items-center">
         {data?.data.map((category: ICategory) => (
           <Card key={category.id} className=" w-full p-5 space-y-2">
             {icons[category.slug as keyof typeof icons] || (
               <FaFile size="24" className="text-green-500" />
             )}
-            <h3 className="font-medium">{category.name}</h3>
+            <h3 className="sm:text-sm lg:font-medium md:font-medium">
+              {category.name}
+            </h3>
             <p className="text-xs">{category?.Files?.length} items</p>
           </Card>
         ))}
