e0af06b72fd40b7f275d261507f46b42b33020e2
Adam Ray <raywibowo68@gmail.com>
2025-01-21 22:13:00 +0700

    feat: Add Pricing link to Navbar for improved navigation

--- src/pages/home/components/Navbar.tsx
+++ src/pages/home/components/Navbar.tsx
@@ -47,6 +47,12 @@ const Navbar = () => {
             >
               Features
             </Link>
+            <a
+              href="#pricing"
+              className="text-gray-500 hover:text-gray-900 px-3 py-2 text-sm font-medium"
+            >
+              Pricing
+            </a>
           </div>
 
           {/* Register Button */}
