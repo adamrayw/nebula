45131463c5bed23397a630b8d509e0de01965264
Adam Ray <raywibowo68@gmail.com>
2025-01-21 22:12:41 +0700

    feat: Add Pricing component to Home page

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -3,6 +3,7 @@ import Features from "./components/Features";
 import Footer from "./components/Footer";
 import Hero from "./components/Hero";
 import Navbar from "./components/Navbar";
+import Pricing from "./components/Pricing";
 
 const Home = () => {
   return (
@@ -10,6 +11,7 @@ const Home = () => {
       <Navbar />
       <Hero />
       <Features />
+      <Pricing />
       <CTA />
       <Footer />
     </>
