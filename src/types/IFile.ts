cfc3222ce5343bba6270e119a1b6ba884dbafb73
Nabil Anugerah <nabilpangestu123@gmail.com>
2025-02-18 20:30:49 +0700

    refactor: Update IFile interface for improved type definitions

--- src/types/IFile.ts
+++ src/types/IFile.ts
@@ -1,26 +1,45 @@
 import { ReactNode } from "react";
 
 interface Starred {
-    id: string;
-    userId: string;
-    fileId: string;
-    createdAt: string;
-    updatedAt: string;
+  id: string;
+  userId: string;
+  fileId: string;
+  createdAt: string;
+  updatedAt: string;
 }
 export interface IFile {
-    length: ReactNode;
-    id: string;
-    name?: string;
-    size?: number;
-    type?: string;
-    file: FileList;
-    originalName?: string;
-    mimeType?: string;
-    location?: string;
-    createdAt?: string;
-    updatedAt?: string;
-    starred: Starred
+  length: ReactNode;
+  id: string;
+  name?: string;
+  size?: number;
+  type?: string;
+  file: FileList;
+  originalName?: string;
+  mimeType?: string;
+  location?: string;
+  createdAt?: string;
+  updatedAt?: string;
+  starred: Starred;
 }
+
+// export interface IFile {
+//   file: {
+//     id: string;
+//     originalName: string;
+//     url: string;
+//     type?: string;
+//     size?: number;
+//     userId: string;
+//     mimeType?: string;
+//     location: string;
+//     categoryId?: string | null;
+//     createdAt: string;
+//     updatedAt: string;
+//     starred: Starred;
+//   } | null;
+//   isOpen: boolean;
+//   onClose: () => void;
+// }
 
 export interface FileIconProps {
   url: string;
