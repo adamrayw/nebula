2c5bca5ae082e163e9142ebb7793f74be4ce0cd3
Adam Ray <raywibowo68@gmail.com>
2025-03-13 06:17:27 +0700

    feat: Update useFetchActivity to support ordering and searching activities

--- src/queries/useFetchActivity.ts
+++ src/queries/useFetchActivity.ts
@@ -2,13 +2,13 @@ import { apiRequest } from "@/api/apiService";
 import { IActivity } from "@/types/IActivity";
 import { useQuery } from "@tanstack/react-query";
 
-export function useFetchActivity(userId: string) {
+export function useFetchActivity(userId: string, order: string, search: string) {
   const activityUrl = import.meta.env.VITE_ACTIVITY_SERVICE_URL;
   return useQuery({
     queryKey: ["activity"],
     queryFn: async () => {
       const response = (await apiRequest(`get`, activityUrl,
-        `/activity/${userId}`
+        `/activity/${userId}?order=${order}&search=${search}`
       )) as {
         data: IActivity[];
       };
