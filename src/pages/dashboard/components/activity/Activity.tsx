f36891d5fe99f2ec46f24f144f7b33a3c9b5c3ad
Adam Ray <raywibowo68@gmail.com>
2025-03-13 06:17:11 +0700

    feat: Add sorting and search functionality to Activity Log

--- src/pages/dashboard/components/activity/Activity.tsx
+++ src/pages/dashboard/components/activity/Activity.tsx
@@ -8,12 +8,18 @@ import {
 } from "@/pages/core/components/design-system/ui/select";
 import { useFetchActivity } from "@/queries/useFetchActivity";
 import { IActivity } from "@/types/IActivity";
+import { useDebounce } from "@uidotdev/usehooks";
 import moment from "moment";
+import { useEffect, useState } from "react";
 
 const Activity = () => {
+  const [order, setOrder] = useState<string>("decending");
+  const [search, setSearch] = useState<string>("");
+  const debounceTerm = useDebounce(search, 300);
+
   const userId = JSON.parse(localStorage.getItem("user")!).id;
 
-  const { data } = useFetchActivity(userId);
+  const { data, refetch } = useFetchActivity(userId, order, search);
 
   const typeColor = (type: string) => {
     switch (type) {
@@ -32,46 +38,49 @@ const Activity = () => {
     }
   };
 
+  useEffect(() => {
+    refetch();
+  }, [order, debounceTerm]);
+
+  const handleOrderChange = (value: string) => {
+    setOrder(value);
+  };
+
+  const handleSearchChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setSearch(e.target.value);
+  }
+
   return (
     <div className="px-10">
       <h1 className="text-2xl font-semibold mb-5">Activity Log</h1>
       <div className="flex items-center justify-between mb-5">
         <div className="flex items-center">
-          <div className="flex items-center">
-            <p className="text-sm font-semibold mr-2 text-nowrap">Sort by:</p>
-            <Select>
-              <SelectTrigger>
-                <SelectValue placeholder="Select date" />
-              </SelectTrigger>
-              <SelectContent>
-                <SelectGroup>
-                  <SelectItem value="today">Today</SelectItem>
-                  <SelectItem value="yesterday">Yesterday</SelectItem>
-                </SelectGroup>
-              </SelectContent>
-            </Select>
-          </div>
-          <div className="flex items-center ml-5">
-            <span className="text-sm font-semibold mr-2">Order:</span>
-            <Select>
-              <SelectTrigger>
-                <SelectValue placeholder="Select order" />
-              </SelectTrigger>
-              <SelectContent>
-                <SelectGroup>
-                  <SelectItem value="ascending">Ascending</SelectItem>
-                  <SelectItem value="descending">Descending</SelectItem>
-                </SelectGroup>
-              </SelectContent>
-            </Select>
-          </div>
+          <span className="text-sm font-semibold mr-2">Order:</span>
+          <Select onValueChange={handleOrderChange} defaultValue={order}>
+            <SelectTrigger>
+              <SelectValue placeholder="Select action" />
+            </SelectTrigger>
+            <SelectContent>
+              <SelectGroup>
+                <SelectItem value="ascending">
+                  Terlama
+                </SelectItem>
+                <SelectItem value="decending">
+                  Terbaru
+                </SelectItem>
+              </SelectGroup>
+            </SelectContent>
+          </Select>
         </div>
         <div className="flex items-center">
           <span className="text-sm font-semibold mr-2">Search:</span>
           <input
             type="text"
-            className="border border-gray-300 rounded-md px-2 py-1"
+            className="border border-gray-300 rounded-md px-4 py-1"
             name="search"
+            value={search}
+            onChange={(e) => handleSearchChange(e)}
+            placeholder="Search by description"
           />
         </div>
       </div>
@@ -82,7 +91,13 @@ const Activity = () => {
             className="flex items-center justify-between py-5 border-b"
           >
             <div className="flex items-center space-x-2">
-              <p className={`text-sm font-semibold uppercase w-fit rounded-full py-1 px-2 ${typeColor(activity.type)})`}>{activity.type}</p>
+              <p
+                className={`text-sm font-semibold uppercase w-fit rounded-full py-1 px-2 ${typeColor(
+                  activity.type
+                )})`}
+              >
+                {activity.type}
+              </p>
               <p className="text-sm text-gray-500">{activity.description}</p>
             </div>
             <p className="text-sm text-gray-500">
